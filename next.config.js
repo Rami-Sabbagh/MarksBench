f9d62fe2984e28523e061896c0a8ba18915fea52
Rami Sabbagh <ramilego4game@gmail.com>
2021-07-11 00:41:43 +0300

    Configure Sass includePaths
    So we can get rid of the relative paths pain

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    // TODO: Document this hack in README.md
+    includePaths: [path.join(__dirname, 'styles/include')],
+  },
 }
