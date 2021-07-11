c436454e0299c49b3b8a2d5de4785d6448a2d41b
Rami Sabbagh <ramilego4game@gmail.com>
2021-07-11 15:00:04 +0300

    Add the applications icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import type { AppProps } from 'next/app'
 import '@styles/globals.scss';
+import type { AppProps } from 'next/app';
+
+import '@lib/icons';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
