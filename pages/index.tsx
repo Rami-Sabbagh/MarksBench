b4381671c30959a59cacfaa1e8ac701f07726c64
Rami Sabbagh <ramilego4game@gmail.com>
2021-07-11 15:52:03 +0300

    Implement the `top-bar` and `bottom-bar`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,39 @@
+import { Icon } from '@iconify/react';
 import styles from '@styles/application.module.scss';
+
+import IconButton from '@components/icon-button';
+
+function Spacer() {
+  return <span className={styles.spacer} />;
+}
 
 export default function Home() {
   return (
     <div className={styles.application}>
       <div className={styles.top_bar}>
-        MarksBench
+        {/* TODO: Get the version out of package.json */}
+        <span className={styles.title}>MarksBench - v1.0.0</span>
+
+        <Spacer />
+
+        <IconButton icon='app:clear-all' />
+        <IconButton icon='app:github' />
+        <IconButton icon='app:help' />
+      </div>
+
+      <Spacer />
+
+      <div className={styles.bottom_bar}>
+        <span className={styles.footer}>
+          Made with <Icon className={styles.heart} icon='app:heart' inline />{' '}
+          by <a href='https://github.com/Rami-Sabbagh/' target='_blank'>Rami Sabbagh</a>
+        </span>
+
+        <Spacer />
+        
+        <IconButton icon='app:table' />
+        <IconButton icon='app:zip' />
+        <IconButton icon='app:add-file' />
       </div>
     </div>
   )
