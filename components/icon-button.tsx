1e360dbe0aab75b066f6065ebe7052b588aad644
Rami Sabbagh <ramilego4game@gmail.com>
2021-07-12 18:39:16 +0300

    Add support for the button disabled state

--- components/icon-button.tsx
+++ components/icon-button.tsx
@@ -2,11 +2,18 @@ import { Icon } from '@iconify/react';
 import styles from './icon-button.module.scss';
 
 type IconButtonProps = {
+    /**
+     * The name of the button's icon.
+     */
     icon: string;
+    /**
+     * Whether the button is disabled or not.
+     */
+    disabled?: boolean;
 };
 
-export default function IconButton({ icon }: IconButtonProps) {
-    return <button className={styles.button} type='button'>
+export default function IconButton({ icon, disabled }: IconButtonProps) {
+    return <button className={styles.button} type='button' disabled={disabled}>
         <Icon icon={icon} />
     </button>;
 }
