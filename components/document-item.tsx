929257bfbd8a16c165322f7bd352405630384fd5
Rami Sabbagh <ramilego4game@gmail.com>
2021-07-17 15:59:13 +0300

    Make `DocumentItem` display the actual entries status

--- components/document-item.tsx
+++ components/document-item.tsx
@@ -1,8 +1,13 @@
+import { useCallback, useEffect, useState } from 'react';
+import numeral from 'numeral';
+
 import { Icon } from '@iconify/react';
 import styles from './document-item.module.scss';
 
 import Spacer from '@components/spacer';
 import IconButton from '@components/icon-button';
+
+import MarksDocument from '@lib/marks-document';
 
 enum ItemState {
     READY = 'ready',
@@ -18,10 +23,43 @@ const stateIcon: Record<ItemState, string> = {
     [ItemState.FAILED]: 'app:file-alert',
 };
 
-export default function DocumentItem() {
-    const state: ItemState = ItemState.READY;
+type DocumentItemProps = {
+    item: MarksDocument;
+};
 
-    const progress = 2/3;
+function getItemState(item: MarksDocument) {
+    if (item.failureReason !== undefined)
+        return ItemState.FAILED;
+    else if (item.processedPages === item.pagesCount)
+        return item.records.length ? ItemState.READY : ItemState.NO_RECORDS;
+    else
+        return ItemState.PROCESSING;
+}
+
+export default function DocumentItem({ item }: DocumentItemProps) {
+    const [state, setState] = useState(ItemState.PROCESSING);
+    const [progress, setProgress] = useState(0);
+    const [subLabel, setSubLabel] = useState('');
+
+    const updateStates = useCallback(() => {
+        setState(getItemState(item));
+        setProgress(item.pagesCount ? item.processedPages / item.pagesCount : 0);
+
+        const recordsCount = numeral(item.records.length).format('0,0');
+        const recordsLabel = `${recordsCount} Record${recordsCount !== '1' ? 's' : ''}`;
+
+        if (item.failureReason) setSubLabel(item.failureReason);
+        else if (item.processedPages === item.pagesCount) setSubLabel(recordsLabel);
+        else if (item.pagesCount === undefined) setSubLabel('Loading document');
+        else setSubLabel(`Processing page ${item.processedPages + 1}/${item.pagesCount}`);
+    }, [item]);
+
+    useEffect(() => {
+        updateStates();
+        item.onUpdate = updateStates;
+        return () => { delete item.onUpdate };
+    }, [updateStates, item]);
+
     const progressPercentage = Math.floor(progress * 10_000) / 100;
 
     return <div className={styles.outer_container} data-state={state}>
@@ -37,13 +75,13 @@ export default function DocumentItem() {
             </div>
 
             <div className={styles.document_label}>
-                1619002423_algorithms and data structures-2-f1-2021.pdf
+                {item.file.name}
             </div>
 
             <Spacer />
 
             <div className={styles.sub_label}>
-                1,500 Record
+                {state === ItemState.NO_RECORDS ? 'No records' : subLabel}
             </div>
 
             {state === ItemState.READY && <IconButton icon='app:download' />}
