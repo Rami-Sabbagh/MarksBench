bb2b6a9b3751a4144c86177fd492dceaf150991d
Rami Sabbagh <ramilego4game@gmail.com>
2021-07-17 15:13:24 +0300

    Set the `pdf.js` `workerSrc` in `_app.tsx`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import '@lib/icons';
 import '@styles/globals.scss';
+
 import type { AppProps } from 'next/app';
 
-import '@lib/icons';
+import { GlobalWorkerOptions } from 'pdfjs-dist';
+
+// TODO: Set the pdf.js workerSrc properly using the router to figure out the baseUrl.
+GlobalWorkerOptions.workerSrc = './pdfjs/pdf.worker.min.js';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
