f32146d7596d4fac7c05d46accbc5c4a74213289
Rami Sabbagh <ramilego4game@gmail.com>
2021-07-17 16:02:19 +0300

    Add `onClick` prop to `IconButton`

--- components/icon-button.tsx
+++ components/icon-button.tsx
@@ -10,10 +10,14 @@ type IconButtonProps = {
      * Whether the button is disabled or not.
      */
     disabled?: boolean;
+    /**
+     * A callback to be triggered when the button is clicked.
+     */
+    onClick?: () => void;
 };
 
-export default function IconButton({ icon, disabled }: IconButtonProps) {
-    return <button className={styles.button} type='button' disabled={disabled}>
+export default function IconButton({ icon, disabled, onClick }: IconButtonProps) {
+    return <button className={styles.button} type='button' disabled={disabled} onClick={onClick}>
         <Icon icon={icon} />
     </button>;
 }
