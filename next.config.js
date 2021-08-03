6bb6e8d7ebecb822b58f8fb8b68a2410f3b64191
Rami Sabbagh <ramilego4game@gmail.com>
2021-08-03 14:08:56 +0300

    Configure `copy-webpack-plugin` for copying the pdfjs worker into the public directory

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
+const CopyPlugin = require('copy-webpack-plugin');
+
 // Extract the version number from package.json
 // so it can be displayed in the application
 const packageJsonPath = path.resolve(__dirname, './package.json');
 const packageJson = fs.readFileSync(packageJsonPath, 'utf-8');
 const version = JSON.parse(packageJson).version || 'UNDEFINED';
+
+const pdfJsWorkerPath = path.resolve(__dirname, './node_modules/pdfjs-dist/build/pdf.worker.min.js');
 
 module.exports = {
   env: {
@@ -15,5 +19,16 @@ module.exports = {
   sassOptions: {
     // TODO: Document this hack in README.md
     includePaths: [path.join(__dirname, 'styles/include')],
+  },
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: pdfJsWorkerPath, to: path.resolve(__dirname, './public/pdfjs/') },
+        ],
+      })
+    );
+
+    return config;
   },
 }
